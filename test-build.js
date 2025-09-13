f8b9491677be372825a567385e6399caeca5a885
Fidi Khouchaba <Assyrian222@hotmail.com>
2025-09-12 20:28:57 -0700

    Add test build script and update build process

---
+++ test-build.js
@@ -0,0 +1,4 @@
+console.log('Test build script running successfully!');
+console.log('Current working directory:', process.cwd());
+console.log('__dirname:', __dirname);
+console.log('Build environment:', process.env.NODE_ENV || 'development');
